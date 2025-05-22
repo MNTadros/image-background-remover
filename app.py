139571aa0f77855f91128b2ff94448de09d9d35b
MNTadros <bobnakhla@gmail.com>
2025-05-22 02:33:06 -0400

    fixed flask port for web hosting

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def analyze():
         return jsonify({ "success": False, "message": str(e) }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
